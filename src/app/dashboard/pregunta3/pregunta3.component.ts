ccb544a73dd9ad0328843a9675ed6d347f61d8c0
chiszo <gigibethandrea@gmail.com>
2024-06-22 21:51:33 -0500

    correcion

--- src/app/dashboard/pregunta3/pregunta3.component.ts
+++ src/app/dashboard/pregunta3/pregunta3.component.ts
@@ -9,16 +9,15 @@ import { MaterialModule } from '../../angular-material/material/material.module'
   styleUrl: './pregunta3.component.css'
 })
 export class Pregunta3Component {
-
+  total: number | null = null;
   submit(cantidadInput: HTMLInputElement):void{
     const cantidad = parseInt(cantidadInput.value);
-    let total = 0;
     if(cantidad<=5) {
-      total = 520;
-      alert(`El total es: ${total}`)
+      this.total = 520;
+      alert(`El total es: ${this.total}`)
     } else {
-      total = (520*0.1)+520
-      alert(`El total es: ${total}`)
+      this.total = (520*0.1)+520
+      alert(`El total es: ${this.total}`)
     }
   }
 }
